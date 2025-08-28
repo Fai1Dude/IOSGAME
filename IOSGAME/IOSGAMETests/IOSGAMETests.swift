d7e2070ce4470a809aa8d159e69db6e4abfc2943
Faisal <FaisalAlhmedi@hotmail.com>
2025-08-28 16:14:55 +0300

    Initial Xcode SpriteKit project setup

---
+++ IOSGAME/IOSGAMETests/IOSGAMETests.swift
@@ -0,0 +1,17 @@
+//
+//  IOSGAMETests.swift
+//  IOSGAMETests
+//
+//  Created by Faisal Alhamdi on 05/03/1447 AH.
+//
+
+import Testing
+@testable import IOSGAME
+
+struct IOSGAMETests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
