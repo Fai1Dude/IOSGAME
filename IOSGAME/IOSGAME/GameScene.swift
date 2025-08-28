11c8188ff67270b2b0897ba2398e06338805a195
Fai1Dude <82786343+Fai1Dude@users.noreply.github.com>
2025-08-28 17:28:16 +0300

    feat: add basic player movement and controls

--- IOSGAME/IOSGAME/GameScene.swift
+++ IOSGAME/IOSGAME/GameScene.swift
@@ -1,109 +1,85 @@
-//
-//  GameScene.swift
-//  IOSGAME
-//
-//  Created by Faisal Alhamdi on 05/03/1447 AH.
-//
-
 import SpriteKit
-import GameplayKit
+import UIKit
 
 class GameScene: SKScene {
-    
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
-    
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func sceneDidLoad() {
+    private var player: SKShapeNode!
+    private let forwardSpeed: CGFloat = 200.0
+    private let jumpHeight: CGFloat = 150.0
+    private var lastUpdateTime: TimeInterval = 0
 
-        self.lastUpdateTime = 0
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+    private var currentLane = 1
+    private let lanePositions: [CGFloat] = [-100, 0, 100]
+
+    override func didMove(to view: SKView) {
+        backgroundColor = .black
+        anchorPoint = CGPoint(x: 0.5, y: 0.5)
+
+        setupLanes()
+        setupPlayer()
+        setupGestures(in: view)
+
+        let cameraNode = SKCameraNode()
+        camera = cameraNode
+        addChild(cameraNode)
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
+
+    func setupLanes() {
+        for x in lanePositions {
+            let lane = SKShapeNode(rectOf: CGSize(width: 80, height: size.height))
+            lane.strokeColor = .darkGray
+            lane.lineWidth = 2
+            lane.position = CGPoint(x: x, y: 0)
+            addChild(lane)
         }
     }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
+
+    func setupPlayer() {
+        player = SKShapeNode(rectOf: CGSize(width: 40, height: 40))
+        player.fillColor = .white
+        player.position = CGPoint(x: lanePositions[currentLane], y: -size.height / 4)
+        addChild(player)
+    }
+
+    func setupGestures(in view: SKView) {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        view.addGestureRecognizer(tap)
+
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
+        swipeLeft.direction = .left
+        view.addGestureRecognizer(swipeLeft)
+
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(handleSwipe(_:)))
+        swipeRight.direction = .right
+        view.addGestureRecognizer(swipeRight)
+    }
+
+    @objc func handleTap() {
+        guard player.action(forKey: "jump") == nil else { return }
+        let jumpUp = SKAction.moveBy(x: 0, y: jumpHeight, duration: 0.2)
+        jumpUp.timingMode = .easeOut
+        let fall = SKAction.moveBy(x: 0, y: -jumpHeight, duration: 0.2)
+        fall.timingMode = .easeIn
+        let sequence = SKAction.sequence([jumpUp, fall])
+        player.run(sequence, withKey: "jump")
+    }
+
+    @objc func handleSwipe(_ gesture: UISwipeGestureRecognizer) {
+        if gesture.direction == .left {
+            currentLane = max(0, currentLane - 1)
+        } else if gesture.direction == .right {
+            currentLane = min(lanePositions.count - 1, currentLane + 1)
         }
+        let move = SKAction.moveTo(x: lanePositions[currentLane], duration: 0.2)
+        player.run(move)
     }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
+
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
+        if lastUpdateTime == 0 { lastUpdateTime = currentTime }
+        let dt = currentTime - lastUpdateTime
+        lastUpdateTime = currentTime
+
+        player.position.y += forwardSpeed * CGFloat(dt)
+        camera?.position = player.position
     }
 }
+
